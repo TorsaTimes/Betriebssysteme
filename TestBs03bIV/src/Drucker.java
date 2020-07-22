dab7c6c6797184d00abf63d8ff0b90bce5144781
tortomas <samuele.tortomasi@hs-furtwangen.de>
2020-07-23 01:07:52 +0200

    Semaphoren Programme

--- TestBs03bIV/src/Drucker.java
+++ TestBs03bIV/src/Drucker.java
@@ -1,20 +1,22 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.concurrent.Semaphore;
+
 class Drucker {
-	
-	synchronized void druckeDatei(String dateiname) {
-		
+	Semaphore s = new Semaphore(1);
+
+	void druckeDatei(String dateiname) {
 		try {
 			BufferedReader in = new BufferedReader(new FileReader(dateiname));
 			String line = in.readLine();
-			
+			s.acquire();
 			while (line != null) {
-				// Zeile line auf dem Drucker ausgeben,
-				// hier simuliert durch Bildschirmausgabe
+// Zeile line auf dem Drucker ausgeben,
+// hier simuliert durch Bildschirmausgabe
 				System.out.println(line);
 				line = in.readLine();
 			}
-			
+			s.release();
 			in.close();
 		} catch (Exception e) {
 			System.out.println("Eine Ausnahme ist aufgetreten.");
